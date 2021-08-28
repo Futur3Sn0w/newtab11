b1391941b2337241449ce68ae50175796d43afcd
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2021-08-28 15:42:16 -0400

    New workings
    Searchbar is now fully functional, placeholder splash image, working menu, placeholder settings panel

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 const btnLeft = document.getElementById('bwLeft');
 const btnRight = document.getElementById('bwRight');
+
 const searchField = document.getElementById("searchField");
+
+const profBtn = document.getElementById("profButton");
+const profMenu = document.getElementById("profMenu");
+const profIcn = document.getElementById("profIconBtn");
+
+const settingsBtn = document.getElementById("settingsBtn");
+const settingsPane = document.getElementById("settingsPanel");
 
 btnRight.onclick = function () {
     document.getElementById('boardwalk').scrollLeft += 230;
@@ -10,10 +18,45 @@ btnLeft.onclick = function () {
     document.getElementById('boardwalk').scrollLeft -= 230;
 };
 
-searchField.onclick = function () {
-    $("#MyLastElement").on('keydown', function (e) {
-        if((e.keyCode==9)&&(!e.shiftKey)) {
-           $(":tabbable").last().focus();
-        }
-      });
+profBtn.onclick = function showHideProfMenu() {
+    if (profMenu.style.transform = "scale(0)") {
+        profMenu.style.transform = "scale(1)";
+        profMenu.style.opacity = "1";
+    } else {
+    profMenu.style.transform = "scale(0)";
+    profMenu.style.opacity = "0";
+}
+}
+
+profBtn.onmousedown = function () {
+    profIcn.style.transform = "scale(.9)";
+}
+
+profBtn.onmouseup = function () {
+    profIcn.style.transform = "scale(1)";
+}
+
+window.addEventListener('mouseup', function(e) {
+    var x = document.querySelector('#profMenu');
+    if (event.target != document.querySelector(".profile")) {
+        x.style.transform = "scale(0)";
+        x.style.opacity = "0";
+    }
+});
+
+$(document).ready(function() {
+$("#searchField").on('keydown', function (e) {
+    if((e.keyCode==9)&&(!e.shiftKey)) {
+       $(":tabbable").last().focus();
+    }
+  });
+});
+
+settingsBtn.onclick = function () {
+    settingsPane.style.bottom = "0";
+    showHideProfMenu();
+}
+
+function closeSettings() {
+    settingsPane.style.bottom = "-100%"
 }
